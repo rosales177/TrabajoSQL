9c29da1726f0e061657c7d3b7694ecef33ee5f37
Geraldo Oneil Rosales Poma <rosalespoma15@gmail.com>
2021-07-22 14:44:30 -0500

    TABLAS y ALTERS terminados

--- implementacion_rhventas.sql
+++ implementacion_rhventas.sql
@@ -1,7 +1,7 @@
 USE master
 go
 
-DROP DATABASE if exists RH_VENTAS
+DROP DATABASE IF EXISTS RH_VENTAS
 go
 
 CREATE DATABASE RH_VENTAS
@@ -23,18 +23,25 @@ LOG ON
 )
 go
 
-DROP TABLE if exists PUESTO
+/*CREACIÓN DE TABLAS*/
+/*PATRON CORESPONDIENTE:
+	PRIMARY KEY = Pk_Nombre del campo
+	FOREIGN KEY = Fk_Tabla de origen_Nombre del campo
+*/
+
+
+DROP TABLE IF EXISTS PUESTO
 go
 CREATE TABLE PUESTO
 (
-	Pk_PuestoId int not null,
+	Pk_Puesto_Id int not null,
 	Nombre nvarchar(40) not null,
 	Salario_Maximo decimal(6,2) not null,
 	Salario_Minimo decimal(4,2) not null
 )
 go
 
-DROP TABLE if exists REGION
+DROP TABLE IF EXISTS REGION
 go
 CREATE TABLE REGION
 (
@@ -43,23 +50,23 @@ CREATE TABLE REGION
 )
 go
 
-DROP TABLE if exists PAIS
+DROP TABLE IF EXISTS PAIS
 go
 
 CREATE TABLE PAIS
 (
 	Pk_Pais_Id int not null,
-	Nombre nvarchar(40) not null,
+	Uk_Nombre nvarchar(40) not null,
 	Fk_Region_RegionId int not null
 )
 go
 
-DROP TABLE if exists SUCURSAL
+DROP TABLE IF EXISTS SUCURSAL
 go
 
 CREATE TABLE SUCURSAL 
 (
-	Pk_SucursalId int not null,
+	Pk_Sucursal_Id int not null,
 	Direccion nvarchar(60) not null,
 	Distrito nvarchar(20) not null,
 	Provincia nvarchar(40) not null,
@@ -86,7 +93,7 @@ CREATE TABLE EMPLEADO
 (
 	Pk_Empleado_Id int not null,
 	Tipo_Doc_Identidad nvarchar(20) not null,
-	Nro_Doc_Identidad nvarchar(20) not null,
+	Uk_Nro_Doc_Identidad nvarchar(20) not null,
 	Nombre nvarchar(40) not null,
 	Apellido nvarchar(40) not null,
 	Email nvarchar(50) not null,
@@ -95,3 +102,222 @@ CREATE TABLE EMPLEADO
 	FSupervisor_Id int not null
 )
 go
+
+DROP TABLE IF EXISTS DEPARTAMENTO
+GO
+CREATE TABLE DEPARTAMENTO
+(
+	Pk_Departamento_ID int not null,
+	Uk_Nombre nvarchar(40) not null,
+	Fk_Sucursal_SucursalId int not null
+)
+GO
+
+DROP TABLE IF EXISTS EMPLEADO_CONTRATOS
+GO
+CREATE TABLE EMPLEADO_CONTRATOS
+(
+	Pk_Contrato_Id int not null,
+	Fk_Empleado_EmpleadoId int not null,
+	Fecha_Inicio date not null,
+	Fecha_Termino date not null,
+	Sueldo_Basico decimal(8,2) not null,
+	Comision_vta decimal(8,2) not null,
+	Fk_Puesto_PuestoId int not null,
+	Fk_Departamento_DepartamentoId int not null
+
+)
+GO
+
+DROP TABLE IF EXISTS EMPLEADO_CONTRATO_HISTORIAL
+GO
+CREATE TABLE EMPLEADO_CONTRATO_HISTORIAL
+(
+	Pk_Empleado_Id int not null,
+	Pk_Contrato_Id int not null,
+	Fecha_Inicio date not null,
+	Fecha_Termino date not null,
+	Sueldo_Basico decimal (6,2) not null,
+	Comision_Vta decimal(5,2)not null,
+	Puesto_Id int not null,
+	Departamento_Id int not null,
+	Años_Servicio int not null,
+	Meses_Servicio int not null,
+	Dias_Servicio int not null
+)
+GO
+
+
+
+/*---PRIMARY KEY---*/
+
+ALTER TABLE PUESTO
+DROP CONSTRAINT IF EXISTS Pk_Puesto_Id
+go
+ALTER TABLE PUESTO
+ADD PRIMARY KEY (Pk_Puesto_Id)
+go
+
+ALTER TABLE REGION
+DROP CONSTRAINT IF EXISTS Pk_Region_Id
+go
+ALTER TABLE REGION
+ADD PRIMARY KEY (Pk_Region_Id)
+go
+
+ALTER TABLE PAIS
+DROP CONSTRAINT IF EXISTS Pk_Pais_Id
+go
+ALTER TABLE PAIS
+ADD PRIMARY KEY (Pk_Pais_Id)
+go
+
+ALTER TABLE SUCURSAL
+DROP CONSTRAINT IF EXISTS Pk_Sucursal_Id
+go
+ALTER TABLE SUCURSAL
+ADD PRIMARY KEY (Pk_Sucursal_Id)
+go
+
+ALTER TABLE DEPARTAMENTO
+DROP CONSTRAINT IF EXISTS Pk_Departamento_Id
+go
+ALTER TABLE DEPARTAMENTO
+ADD PRIMARY KEY (Pk_Departamento_Id)
+go
+
+ALTER TABLE USUARIO
+DROP CONSTRAINT IF EXISTS Pk_Nombre
+go
+ALTER TABLE USUARIO
+ADD PRIMARY KEY (Pk_Nombre)
+go
+
+ALTER TABLE EMPLEADO
+DROP CONSTRAINT IF EXISTS Pk_Empleado_Id
+go
+ALTER TABLE EMPLEADO
+ADD PRIMARY KEY (Pk_Empleado_Id)
+go
+
+ALTER TABLE EMPLEADO_CONTRATOS
+DROP CONSTRAINT if exists Pk_Contrato_Id
+go
+ALTER TABLE EMPLEADO_CONTRATOS
+ADD PRIMARY KEY (Pk_Contrato_Id)
+go
+
+ALTER TABLE EMPLEADO_CONTRATO_HISTORIAL
+DROP CONSTRAINT if exists Pk_EmpleadoId_ContratoId
+go
+ALTER TABLE EMPLEADO_CONTRATO_HISTORIAL
+ADD CONSTRAINT Pk_EmpleadoId_ContratoId PRIMARY KEY (Pk_Empleado_Id, Pk_Contrato_Id)
+go
+
+
+
+/*---FOREIGN KEYS---*/
+
+ALTER TABLE PAIS
+DROP CONSTRAINT IF EXISTS Fk_Region_RegionId
+go
+ALTER TABLE PAIS
+ADD CONSTRAINT Fk_Region_RegionId FOREIGN KEY(Fk_Region_RegionId)
+REFERENCES REGION(Pk_Region_Id)
+go
+
+ALTER TABLE SUCURSAL
+DROP CONSTRAINT IF EXISTS Fk_Pais_PaisId
+go
+ALTER TABLE SUCURSAL
+ADD CONSTRAINT Fk_Pais_PaisId FOREIGN KEY(Fk_Pais_PaisId)
+REFERENCES PAIS(Pk_Pais_Id)
+go
+
+ALTER TABLE DEPARTAMENTO
+DROP CONSTRAINT IF EXISTS Fk_Sucursal_SucursalId
+go
+ALTER TABLE DEPARTAMENTO
+ADD CONSTRAINT Fk_Sucursal_SucursalId FOREIGN KEY(Fk_Sucursal_SucursalId)
+REFERENCES SUCURSAL(Pk_SucursalId)
+go
+
+ALTER TABLE EMPLEADO
+DROP CONSTRAINT IF EXISTS Fk_Empleado_SupervisorId
+go
+ALTER TABLE EMPLEADO
+ADD CONSTRAINT Fk_Empleado_SupervisorId FOREIGN KEY(FSupervisor_Id)
+REFERENCES EMPLEADO(Pk_Empleado_Id)
+go
+
+ALTER TABLE USUARIO
+DROP CONSTRAINT IF EXISTS Fk_Empleado_EmpleadoId
+go
+ALTER TABLE USUARIO
+ADD CONSTRAINT Fk_Empleado_EmpleadoId FOREIGN KEY(Fk_Empleado_EmpleadoId)
+REFERENCES EMPLEADO(Pk_Empleado_Id)
+ON DELETE CASCADE	------->  RESTRICCION ELIMINACION EN CASCADA
+go
+
+ALTER TABLE EMPLEADO_CONTRATOS
+DROP CONSTRAINT if  exists Fk_Empleado_EmpleadoId
+go
+ALTER TABLE EMPLEADO_CONTRATOS
+ADD CONSTRAINT Fk_Empleado_EmpleadoId FOREIGN KEY(Fk_Empleado_EmpleadoId)
+REFERENCES EMPLEADO(Pk_Empleado_Id)
+go
+
+ALTER TABLE EMPLEADO_CONTRATOS
+DROP CONSTRAINT if  exists Fk_Departamento_DepartamentoId
+go
+ALTER TABLE EMPLEADO_CONTRATOS
+ADD CONSTRAINT Fk_Departamento_DepartamentoId FOREIGN KEY(Fk_Departamento_DepartamentoId)
+REFERENCES DEPARTAMENTO(Pk_Departamento_Id)
+go
+
+/*---UNIQUE KEY---*/
+
+ALTER TABLE PAIS
+drop constraint IF EXISTS Uk_NombreP
+GO
+ALTER TABLE PAIS
+add constraint Uk_NombreP Unique (Uk_Nombre)
+GO
+
+ALTER TABLE DEPARTAMENTO
+drop constraint IF EXISTS Uk_Nombre
+GO
+ALTER TABLE DEPARTAMENTO
+add constraint Uk_Nombre Unique (Uk_Nombre)
+GO
+ALTER TABLE EMPLEADO
+drop constraint IF EXISTS Uk_Nro_Doc_Identidad
+GO
+ALTER TABLE EMPLEADO
+add constraint Uk_Nro_Doc_Identidad Unique (Uk_Nro_Doc_Identidad)
+GO
+
+
+
+/*---ALTER CHECK---*/
+
+ALTER TABLE EMPLEADO
+drop constraint IF EXISTS Ck_Nro_Doc_Identidad
+GO
+ALTER TABLE EMPLEADO
+add constraint Ck_Nro_Doc_Identidad CHECK (Uk_Nro_Doc_Identidad not like '%[^0-9]%')
+GO
+
+ALTER TABLE EMPLEADO
+drop constraint IF EXISTS Ck_Email
+GO
+ALTER TABLE EMPLEADO
+add constraint Ck_Email CHECK (Email like '%[^@]@%[^.].[a-z][a-z][a-z]')
+GO
+
+ALTER TABLE EMPLEADO
+drop constraint IF EXISTS Ck_Telefono
+GO
+ALTER TABLE EMPLEADO
+add constraint Ck_Telefono CHECK (Telefono not like '%[^0-9]%')
+GO
